96061ac09c8efd6da8abd5499d60791ac064def6
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2020-09-21 12:24:32 -0400

    add validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,17 @@
 class Listing < ApplicationRecord
   has_one_attached :image
+  validates :name, :description, :price, presence:true
+  validates :price, numericality: {greater_than: 0}
+
+  validate :correct_image_type
+
+  private
+
+  def correct_image_type
+    if image.attached? && !image.content_type.in?(%w(image/png image/jpeg image/jpg))
+      errors.add(:image, "must be a jpeg or png file!")
+    elsif image.attached? == false
+      errors.add(:image, "must be attached!")
+    end
+  end
 end
