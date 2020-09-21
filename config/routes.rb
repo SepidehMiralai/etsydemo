fcb415ba63e4895fca0bae7f815829ee2dbae884
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2020-09-21 10:05:17 -0400

    install and set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :listings
   get 'pages/about'
   get 'pages/contact'
+  # get '/users',   to: 'users#index'
   root 'listings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
